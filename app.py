97036e7beae457bd68d9f7ea203583a473ec9eae
steffin.codes <steffin.codes@gmail.com>
2023-07-31 21:12:01 -0400

    Added File Support and Cleaned up

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from connection import KaggleDatasetConnection
 import streamlit as st
-
 st.set_page_config(page_title="Kaggle-Streamlit Connection Demo App")
 st.title("Kaggle-Streamlit Connector App")
 st.info(
@@ -8,22 +7,26 @@ st.info(
 )
 st.subheader("Loading dataset from Kaggle Through API")
 conn = st.experimental_connection("kaggle_datasets", type=KaggleDatasetConnection)
-# dataset_path = st.text_input(
-#     "Kaggle Link Of Dataset", "varsharam/walmart-sales-dataset-of-45stores"
-# )
-# dataset_name = st.text_input(
-#     "Kaggle Dataset Name", "walmart-sales-dataset-of-45stores.csv"
-# )
 dataset_path = st.text_input(
-    "Kaggle Link Of Dataset", "rmisra/news-headlines-dataset-for-sarcasm-detection"
+    "Kaggle Link Of Dataset", "varsharam/walmart-sales-dataset-of-45stores"
 )
-dataset_name = st.text_input(
-    "Kaggle Dataset Name", "Sarcasm_Headlines_Dataset.json"
-)
-if st.button("Load Data"):
+file_list = ""
+if dataset_path.count("/") == 1:
+    owner, dataset = dataset_path.split("/")
     try:
-        df = conn.get(path=dataset_path, filename=dataset_name, ttl=3600)
-        st.write("Data Preview:")
-        st.dataframe(df.head(20))
+        file_list = conn.list(path=dataset_path, ttl=3600)
+        st.json(file_list)
     except Exception as e:
-        st.error(f"Oops...{e.args}")
+        st.error(f"Oops...{e.__class__}")
+if file_list:
+    dataset_name = st.text_input(
+        "Kaggle Dataset Name", file_list[0]
+    ).replace("\"","")
+    if dataset_name:
+        if st.button("Load Data"):
+            try:
+                df = conn.get(path=dataset_path, filename=dataset_name, ttl=3600)
+                st.write(f"{dataset_name} preview")
+                st.dataframe(df.head(20))
+            except Exception as e:
+                st.error(f"Oops...{e.__class__}")
